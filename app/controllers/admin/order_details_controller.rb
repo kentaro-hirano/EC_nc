17c181db3a774f58f301d826b0d77ab9fee48226
EC2 Default User <yakyukentaro@gmail.com>
2021-02-16 09:32:16 +0000

    create admin_orders_path status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,14 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    # binding.pry
+    @order = @order_detail.order
+      # binding.pry
+    if params[:order_detail][:production_status] = "2"
+      @order_detail.order.update(order_status: 2)
+    end
+    if @order.order_details.where(production_status: 3).count == @order.order_details.count
+      @order_detail.order.update(order_status: 3)
+    end
     flash[:success] = "制作ステータスを更新しました"
     redirect_to admin_order_path(@order_detail.order)
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,15 +7,19 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def update
     @order = Order.find(params[:id])
-    binding.pry
     @order.update(order_params)
+    if params[:order][:order_status] = "1"
+      @order.order_details.each do |order_detail|
+        order_detail.update(production_status: 1)
+      end
+    end
     flash[:success] = "注文ステータスを更新しました"
     redirect_to admin_order_path(@order)
   end
-  
+
   private
   def order_params
     params.require(:order).permit(:order_status)
