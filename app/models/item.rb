00ccc4f133ddc161bb76f95581eb66a7c7d8f31c
EC2 Default User <yakyukentaro@gmail.com>
2021-02-17 09:35:27 +0000

    create search_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   
   belongs_to :genre, optional: true
   has_many :cart_items
+  
+  def self.search(search)
+    if search
+      Item.where(['name LIKE ?', "%#{search}%"])
+    else
+      Item.all
+    end
+  end
 end
