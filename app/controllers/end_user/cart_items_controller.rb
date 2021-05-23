001011e180301ff11557b8ec47b0160763e37fb4
EC2 Default User <yakyukentaro@gmail.com>
2021-05-23 14:49:48 +0000

    change cart_items_controller.rb

--- app/controllers/end_user/cart_items_controller.rb
+++ app/controllers/end_user/cart_items_controller.rb
@@ -8,34 +8,46 @@ class EndUser::CartItemsController < ApplicationController
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.end_user_id = current_end_user.id
     @update_cart_item = CartItem.find_by(item_id: @cart_item.item_id)
-    
+
     if @update_cart_item.present?
-      @cart_item.amount += @update_cart_item.amount
-      @update_cart_item.destroy
-    end
-    
-    if @cart_item.save
+      @update_num = @update_cart_item.amount += @cart_item.amount
+      @update_cart_item.update(amount: @update_num)
+      redirect_to cart_items_path
+    elsif @cart_item.save
       redirect_to cart_items_path
     else
       @item = Item.find(params[:cart_item][:item_id])
       @cart_item = CartItem.new
       render 'end_user/items/show'
     end
+    
+    # if @update_cart_item.present?
+    #   @cart_item.amount += @update_cart_item.amount
+    #   @update_cart_item.destroy
+    # end
+
+    # if @cart_item.save
+    #   redirect_to cart_items_path
+    # else
+    #   @item = Item.find(params[:cart_item][:item_id])
+    #   @cart_item = CartItem.new
+    #   render 'end_user/items/show'
+    # end
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     flash[:success] = "商品を削除しました"
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     @cart_items = current_end_user.cart_items
     @cart_items.destroy_all
